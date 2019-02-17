4c528d03cb4d90ada920bf6f83ea757a77c92d91
ikovic <kovic.ivan@gmail.com>
2019-02-17 23:18:59 +0100

    Send email if there are items in stock

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const nodemailer = require('nodemailer');
 
-const scrapeForInStockItem = (url, html) => {
+const sendEmail = async html => {
+  try {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let account = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: account.user, // generated ethereal user
+        pass: account.pass, // generated ethereal password
+      },
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+      from: 'test123@mailinator.com', // sender address
+      to: 'test123@mailinator.com', // list of receivers
+      subject: 'LotR items in stock!', // Subject line
+      html,
+    };
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail(mailOptions);
+
+    console.log('Message sent: %s', info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const createEmail = inStockUrls => {
+  return `<ul>${inStockUrls.reduce((string, url) => {
+    return `${string}<li>${url}</li>`;
+  }, '')}</ul>`;
+};
+
+const isItemInStock = html => {
   const $ = cheerio.load(html);
-  const itemInStock = !$('img[src="v8outofstock.gif"]').length;
-  console.log(url, itemInStock);
+  return !$('img[src="v8outofstock.gif"]').length;
 };
 
 const urls = [
@@ -15,11 +57,20 @@ const urls = [
 const promises = urls.map(url => axios.get(url));
 
 Promise.all(promises).then(responses => {
-  responses.forEach(response => {
+  const itemsInStock = responses.reduce((items, response) => {
     if (response.status !== 200) {
-      return;
+      return items;
     }
 
-    scrapeForInStockItem(response.config.url, response.data);
-  });
+    if (!isItemInStock(response.data)) {
+      return items;
+    }
+
+    return [...items, response.config.url];
+  }, []);
+
+  if (itemsInStock.length) {
+    console.log('There are items in stock!');
+    sendEmail(createEmail(itemsInStock));
+  }
 });
